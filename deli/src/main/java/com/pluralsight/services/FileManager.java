9678fdf71eaf9a67f6aedb772605f507c31ffcbc
fahd209 <166452601+fahd209@users.noreply.github.com>
2026-01-03 21:43:07 -0800

    Update log message for saving receipt

--- deli/src/main/java/com/pluralsight/services/FileManager.java
+++ deli/src/main/java/com/pluralsight/services/FileManager.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 public class FileManager {
     public static void saveReceipt(Order order)
     {
-        System.out.println("Saving receipt to your speciefied google account.");
+        System.out.println("Saving receipt to your speciefied folder");
         String receiptDate = order.getOrderDate().replace(" ", "").replace(":", "-");
         File file = new File("receipts\\"+receiptDate+".txt");
 
